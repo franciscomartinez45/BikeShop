55f5bbe50b6761252934b6f59a3b1f0e88a3cca8
fmartinez <fmartinez110@toromail.csudh.edu>
2024-12-05 18:29:02 -0800

    FIXES

--- BikeShop/src/BikeShopMainGUI.java
+++ BikeShop/src/BikeShopMainGUI.java
@@ -1,12 +1,10 @@
+
 import javax.swing.*;
 import java.awt.event.*;
-import java.sql.SQLException;
-import java.util.Map;
 import java.awt.*;
 import javax.swing.table.*;
 
-
-public class BikeShopMainGUI{
+public class BikeShopMainGUI {
 
     private JTextField searchField;
     private JComboBox<String> columnSelect;
@@ -17,118 +15,102 @@ public class BikeShopMainGUI{
     private String password;
     private JFrame frame;
     private GridBagConstraints gbc;
-   
+
     public BikeShopMainGUI(String username, String password) {
         frame = new JFrame("Bike Shop Database");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(1090, 600);
         frame.setLocationRelativeTo(null);
-        
+
         this.username = username;
         this.password = password;
         frame.setLayout(new GridBagLayout());
         initializeUI();
         frame.setVisible(true);
     }
- 
-    private void initializeUI(){
 
-        String[] tables = {"bike_model","bike_shipment","bike_type","customer","manufacturer","purchase"};
-       
+    private void initializeUI() {
+
+        String[] tables = {"bike_model", "bike_shipment", "bike_type", "customer", "manufacturer", "purchase"};
+
         gbc = new GridBagConstraints();
         tableSelect = new JComboBox<String>(tables);
-        columnSelect = new JComboBox<String>(BikeShopSQL.getColumns("purchase", username, password));
+        columnSelect = new JComboBox<String>(BikeShopSQL.getColumns("bike_model", username, password));
         searchField = new JTextField();
         JButton searchB = new JButton("Search");
-        
-        dataTable = new JTable(BikeShopSQL.tableLookup("customer", "","", username, password));
+
+        dataTable = new JTable(BikeShopSQL.tableLookup("bike_model", "", "", username, password));
         dataTable.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
         TableColumnModel columnModel = dataTable.getColumnModel();
         dataTable.setColumnSelectionAllowed(false);
-        for(int i = 0; i < dataTable.getColumnCount(); i++){
-            int estimate = ((String)dataTable.getModel().getValueAt(1, i)).length();
-    
-            if (estimate < 15){
+        for (int i = 0; i < dataTable.getColumnCount(); i++) {
+            int estimate = ((String) dataTable.getModel().getValueAt(1, i)).length();
+
+            if (estimate < 15) {
                 columnModel.getColumn(i).setPreferredWidth(150);
-            }
-            else{
+            } else {
                 columnModel.getColumn(i).setPreferredWidth(300);
             }
-        
+
         }
-        System.out.println(columnSelect);
+
         scroll = new JScrollPane(dataTable);
         scroll.setVisible(true);
-        // scroll.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+        scroll.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
         scroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
         JLabel resultsLabel = new JLabel("Results:");
         JLabel searchLabel = new JLabel("Enter Query:");
-        JLabel columnLabel = new JLabel("Select Table:");
-        JLabel tableLabel = new JLabel("Select Column:");
-        JButton addB = new JButton("Add Data");
-        JButton removeB = new JButton("Remove Data");
-        JButton updateB = new JButton("Update Data");
+        JLabel columnLabel = new JLabel("Select Attribute:");
+        JLabel tableLabel = new JLabel("Select Table:");
         JButton refreshB = new JButton("Refresh Data");
-        
-        gbc.fill = GridBagConstraints.HORIZONTAL;  
-        gbc.gridx = 0;  
+
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.gridx = 0;
         gbc.gridy = 0;
         frame.add(tableLabel, gbc);
-        gbc.gridx = 1;  
+        gbc.gridx = 1;
         gbc.gridy = 0;
         frame.add(columnLabel, gbc);
-        gbc.gridx = 2;  
+        gbc.gridx = 2;
         gbc.gridy = 0;
         frame.add(searchLabel, gbc);
-        gbc.fill = GridBagConstraints.HORIZONTAL;  
-        gbc.gridx = 0;  
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.gridx = 0;
         gbc.gridy = 1;
         gbc.weightx = 0.1;
         frame.add(tableSelect, gbc);
-        gbc.gridx = 1;  
+        gbc.gridx = 1;
         gbc.gridy = 1;
         gbc.weightx = 0.3;
         frame.add(columnSelect, gbc);
-        gbc.gridx = 2;  
+        gbc.gridx = 2;
         gbc.gridy = 1;
         gbc.weightx = 0.5;
         frame.add(searchField, gbc);
         gbc.weightx = 0;
-        gbc.gridx = 3;  
+        gbc.gridx = 3;
         gbc.gridy = 1;
         frame.add(searchB, gbc);
-        gbc.fill = GridBagConstraints.HORIZONTAL; 
+        gbc.fill = GridBagConstraints.HORIZONTAL;
 
-        gbc.gridx = 0;  
+        gbc.gridx = 0;
         gbc.gridy = 2;
         frame.add(resultsLabel, gbc);
-        gbc.fill = GridBagConstraints.HORIZONTAL;  
-        gbc.gridx = 0;  
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.gridx = 0;
         gbc.gridy = 3;
         gbc.gridwidth = 5;
         gbc.weightx = 1;
         frame.add(scroll, gbc);
-        gbc.fill = GridBagConstraints.HORIZONTAL;  
+        gbc.fill = GridBagConstraints.HORIZONTAL;
         gbc.gridwidth = 1;
         gbc.weightx = 0;
-        gbc.gridx = 0;  
+        gbc.gridx = 0;
         gbc.gridy = 16;
-        frame.add(updateB, gbc);
-        gbc.gridx = 1;  
-        gbc.gridy = 16;
-        frame.add(addB, gbc);
-        gbc.gridx = 2;  
-        gbc.gridy = 16;
-        frame.add(removeB, gbc);
-        gbc.gridx = 3;  
-        gbc.gridy = 16;
-        
+
         frame.add(refreshB, gbc);
         gbc.gridx = 5;
         gbc.gridy = 16;
-        
-
-      
 
         searchB.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
@@ -148,39 +130,19 @@ public class BikeShopMainGUI{
             }
         });
 
-        addB.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                add();
-            }
-        });
-
-        removeB.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                remove();
-            }
-        });
-
-      
-        updateB.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                
-            }
-        });
     }
 
-    public void insertTable(){
+    public void insertTable() {
         dataTable.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
         TableColumnModel columnModel = dataTable.getColumnModel();
-        for(int i = 0; i < dataTable.getColumnCount(); i++){
-            if(dataTable.getRowCount() <= 0){
+        for (int i = 0; i < dataTable.getColumnCount(); i++) {
+            if (dataTable.getRowCount() <= 0) {
                 columnModel.getColumn(i).setPreferredWidth(150);
-            }
-            else{
-                int estimate = ((String)dataTable.getModel().getValueAt(0, i)).length();
-                if (estimate < 15){
+            } else {
+                int estimate = ((String) dataTable.getModel().getValueAt(0, i)).length();
+                if (estimate < 15) {
                     columnModel.getColumn(i).setPreferredWidth(150);
-                }
-                else{
+                } else {
                     columnModel.getColumn(i).setPreferredWidth(300);
                 }
             }
@@ -189,129 +151,47 @@ public class BikeShopMainGUI{
         scroll.setVisible(true);
         scroll.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
         scroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
-        gbc.fill = GridBagConstraints.HORIZONTAL;  
-        gbc.gridx = 0;  
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.gridx = 0;
         gbc.gridy = 3;
         gbc.gridwidth = 5;
         gbc.weightx = 1;
         frame.add(scroll, gbc);
     }
-   
-    public void refresh(){
-            String table = tableSelect.getSelectedItem().toString();
-           
-            frame.remove(this.scroll);
-            frame.remove(this.columnSelect);
-            columnSelect = new JComboBox<String>(BikeShopSQL.getColumns(table, username, password));
-            columnSelect.setBounds(180,100,120,40);
-            dataTable = new JTable(BikeShopSQL.tableLookup(table, "", "", username, password));
-            gbc.gridx = 1;  
-            gbc.gridy = 1;
-            gbc.gridwidth = 1;
-            gbc.weightx = 0.3;
-            frame.add(columnSelect, gbc);
-            insertTable();
-            frame.revalidate();
-            frame.repaint();
+
+    public void refresh() {
+        String table = tableSelect.getSelectedItem().toString();
+
+        frame.remove(this.scroll);
+        frame.remove(this.columnSelect);
+        columnSelect = new JComboBox<String>(BikeShopSQL.getColumns(table, username, password));
+        columnSelect.setBounds(180, 100, 120, 40);
+        dataTable = new JTable(BikeShopSQL.tableLookup(table, "", "", username, password));
+        gbc.gridx = 1;
+        gbc.gridy = 1;
+        gbc.gridwidth = 1;
+        gbc.weightx = 0.3;
+        frame.add(columnSelect, gbc);
+        insertTable();
+        frame.revalidate();
+        frame.repaint();
     }
 
-    public void search(){
+    public void search() {
         String keyword = searchField.getText();
         String column = columnSelect.getSelectedItem().toString();
         String table = tableSelect.getSelectedItem().toString();
-    
-        try{
+
+        try {
             frame.remove(this.scroll);
             dataTable = new JTable(BikeShopSQL.tableLookup(table, column, keyword, username, password));
             insertTable();
             frame.revalidate();
             frame.repaint();
-        }
-        catch(Exception e){
+        } catch (Exception e) {
             searchField.setText("Error searching for items: " + e.getMessage());
             refresh();
         }
     }
 
-    public void add(){
-        
-        JFrame addFrame = new JFrame("Insert Into Database");
-        addFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        addFrame.setSize(400, 650);
-        addFrame.setLocationRelativeTo(null);
-        addFrame.setLayout(new GridBagLayout());
-        addFrame.setVisible(true);
-    }
-
-    public void remove(){
-        String table = tableSelect.getSelectedItem().toString();
-        int[] rows = dataTable.getSelectedRows();
-        String column = columnSelect.getItemAt(0);
-        for(int row: rows){
-            String value = (String) dataTable.getValueAt(row, 0);
-            BikeShopSQL.remove(table, column, value, username, password);
-        }
-        refresh();
-        
-    }
-
-    //Create JFrame
-    public void service(String patientID) {
-    JFrame serviceFrame = new JFrame("Select a Telehealth service");
-    serviceFrame.setSize(400, 400);
-    serviceFrame.setLocationRelativeTo(null);
-    serviceFrame.setLayout(new GridBagLayout());
-
-    GridBagConstraints gbcservice = new GridBagConstraints();
-    gbcservice.fill = GridBagConstraints.HORIZONTAL;
-    gbcservice.insets = new Insets(10, 10, 10, 10);
-    
-    //Display the patient information
-    JLabel information = new JLabel("Patient information:");
-    gbcservice.gridx = 0;
-    gbcservice.gridy = 0;
-    gbcservice.gridwidth = 2;
-    serviceFrame.add(information, gbcservice);
-
-    try {
-        //Retrieve the patient information
-        Map<String, String> data = BikeShopSQL.getPatientColumn(patientID, username,password); 
-        if (data != null) {
-            int row = 1;
-            //Map to iterate through key, value pairs
-           for (Map.Entry<String, String> entry : data.entrySet()) {
-                gbcservice.gridwidth = 1; 
-                gbcservice.gridx = 0;
-                gbcservice.gridy = row;
-                serviceFrame.add(new JLabel(entry.getKey() + ":"), gbcservice);
-                gbcservice.gridx = 1;
-                serviceFrame.add(new JLabel(entry.getValue()), gbcservice);
-                row++;
-            }
-
-        } else {
-            gbcservice.gridwidth = 2;
-            gbcservice.gridx = 0;
-            gbcservice.gridy = 1;
-            serviceFrame.add(new JLabel("No patient found with ID: " + patientID), gbcservice);
-        }
-    } catch (ClassNotFoundException | SQLException e) {
-        e.printStackTrace();
-        gbcservice.gridwidth = 2;
-        gbcservice.gridx = 0;
-        gbcservice.gridy = 1;
-        serviceFrame.add(new JLabel("Error fetching patient data"), gbcservice);
-    }
-    //Button to assign a Telehealth Service
-    JButton addAppointmentButton = new JButton("Add Appointment");
-    addAppointmentButton.addActionListener(e -> {
-        
-    });
-    
-    gbcservice.gridwidth = 2;
-    gbcservice.gridx = 0;
-    gbcservice.gridy++;
-    serviceFrame.add(addAppointmentButton, gbcservice);
-    serviceFrame.setVisible(true);
-}}
-
+}
--- BikeShop/src/BikeShopSQL.java
+++ BikeShop/src/BikeShopSQL.java
@@ -1,150 +1,71 @@
-import java.sql.*;
-import java.util.LinkedHashMap;
-import java.util.Map;
 
+import java.sql.*;
 import javax.swing.table.DefaultTableModel;
 
+public class BikeShopSQL {
 
-public class BikeShopSQL{
-
-    public static boolean testConnection(String user, char[] pass){
+    public static boolean testConnection(String user, char[] pass) {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             String url = "jdbc:mysql://localhost:3306/bike_shop";
             String password = "";
-            for(char c: pass){
+            for (char c : pass) {
                 password += c;
             }
-            Connection connection = DriverManager.getConnection(url, user,password);
+            Connection connection = DriverManager.getConnection(url, user, password);
 
-            System.out.println("Connection Secured");
-            connection.close();
-            return(true);
+            return (true);
 
-        }catch (Exception e) {
+        } catch (Exception e) {
             System.out.println(e.toString());
-        
-            return(false);
+
+            return (false);
         }
     }
 
-    public static String[] getColumns(String table, String username, String password){
+    public static String[] getColumns(String table, String username, String password) {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/bike_shop", username, password);
-            String query = "SELECT * FROM "+table;
+            String query = "SELECT * FROM " + table;
             PreparedStatement preparedStatement = connection.prepareStatement(query);
             ResultSet resultSet = preparedStatement.executeQuery();
             ResultSetMetaData resultMetaData = resultSet.getMetaData();
             String[] columns = new String[resultMetaData.getColumnCount()];
-            for(int i = 0; i < resultMetaData.getColumnCount(); i++){
-                columns[i] = resultMetaData.getColumnName(i+1);
+            for (int i = 0; i < resultMetaData.getColumnCount(); i++) {
+                columns[i] = resultMetaData.getColumnName(i + 1);
             }
             resultSet.close();
             preparedStatement.close();
             connection.close();
-            return(columns);
+            return (columns);
 
-        }catch (Exception e) {
-            System.out.println("ERROR: "+e.getMessage());
-            return(null);
+        } catch (Exception e) {
+            System.out.println("ERROR: " + e.getMessage());
+            return (null);
         }
     }
 
-    //Get patient data, returned as a key,value pair 
-   public static Map<String, String> getPatientColumn(String patientID, String username, String password) throws ClassNotFoundException, SQLException {
-        Map<String, String> patientData = new LinkedHashMap<>();
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/CipherCare", username, password);
-        String query = "SELECT * FROM purchase WHERE purchaseID = ?";
-        PreparedStatement statement = connection.prepareStatement(query);
-        statement.setString(1, patientID);
-        ResultSet resultSet = statement.executeQuery();
-        if (resultSet.next()) {
-            ResultSetMetaData metaData = resultSet.getMetaData();
-            int columnCount = metaData.getColumnCount();
-            for (int i = 1; i <= columnCount; i++) {
-                String columnName = metaData.getColumnName(i);
-                String columnValue = resultSet.getString(i);
-                patientData.put(columnName, columnValue);
-            }
-        }   
-        resultSet.close();
-        statement.close();
-        connection.close();
-
-        return patientData.isEmpty() ? null : patientData;
-}
-
-
-// //Returns all the available telehealth services
-// public static List<String> getTelehealthServices(String username, String password) throws ClassNotFoundException, SQLException {
-//     List<String> services = new ArrayList<>();
-    
-//     Class.forName("com.mysql.cj.jdbc.Driver");
-//     Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/CipherCare", username, password);
-//     String query = "SELECT name FROM Telehealth";
-//     PreparedStatement statement = connection.prepareStatement(query);
-//     ResultSet resultSet = statement.executeQuery();
-    
-//     while (resultSet.next()) {
-//         services.add(resultSet.getString("name"));
-//     }
-    
-//     resultSet.close();
-//     statement.close();
-//     connection.close();
-    
-//     return services;
-
-// }
-
-// //Insert appointment into database
-// public static void saveAppointment(String patientID, String telehealthService, String date, String startTime, String endTime, String username, String password) throws ClassNotFoundException, SQLException {
-//     Class.forName("com.mysql.cj.jdbc.Driver");
-//     Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/CipherCare", username, password);
-//     String serviceQuery = "SELECT telehealthID FROM Telehealth WHERE name = ?";
-//     PreparedStatement serviceStatement = connection.prepareStatement(serviceQuery);
-//     serviceStatement.setString(1, telehealthService);
-//     ResultSet serviceResultSet = serviceStatement.executeQuery();
-//     int telehealthID = 0;
-//     if (serviceResultSet.next()) {
-//         telehealthID = serviceResultSet.getInt("telehealthID");
-//     }
-//     String insertQuery = "INSERT INTO Appointment (recordID, telehealthID, date, startTime, endTime) VALUES (?, ?, ?, ?, ?)";
-//     PreparedStatement insertStatement = connection.prepareStatement(insertQuery);
-//     insertStatement.setInt(1, Integer.parseInt(patientID)); 
-//     insertStatement.setInt(2, telehealthID);
-//     insertStatement.setDate(3, Date.valueOf(date));
-//     insertStatement.setString(4, startTime);
-//     insertStatement.setString(5, endTime);
-//     insertStatement.executeUpdate();
-//     serviceResultSet.close();
-//     serviceStatement.close();
-//     insertStatement.close();
-//     connection.close();
-// }
-
-    public static DefaultTableModel tableLookup(String table, String column, String keyword, String username, String password){
+    public static DefaultTableModel tableLookup(String table, String column, String keyword, String username, String password) {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/bike_shop", username, password);
-            String query = "SELECT * FROM "+ table;
-            if (!(keyword.equals("")||column.equals(""))){
-                query = query.concat(" WHERE "+column+" IN("+keyword+")");
+            String query = "SELECT * FROM " + table;
+            if (!(keyword.equals("") || column.equals(""))) {
+                query = query.concat(" WHERE " + column + " IN(" + keyword + ")");
             }
             PreparedStatement preparedStatement = connection.prepareStatement(query);
             ResultSet resultSet = preparedStatement.executeQuery();
             ResultSetMetaData resultMetaData = resultSet.getMetaData();
             String[] columns = new String[resultMetaData.getColumnCount()];
-            for(int i = 0; i < resultMetaData.getColumnCount(); i++){
-                columns[i] = resultMetaData.getColumnName(i+1);
+            for (int i = 0; i < resultMetaData.getColumnCount(); i++) {
+                columns[i] = resultMetaData.getColumnName(i + 1);
             }
             DefaultTableModel model = new DefaultTableModel();
             model.setColumnIdentifiers(columns);
-            while(resultSet.next()){
+            while (resultSet.next()) {
                 Object[] values = new Object[columns.length];
-                for(int i = 0; i < columns.length; i++){
+                for (int i = 0; i < columns.length; i++) {
                     values[i] = resultSet.getString(columns[i]);
                 }
                 model.addRow(values);
@@ -153,32 +74,12 @@ public class BikeShopSQL{
             resultSet.close();
             preparedStatement.close();
             connection.close();
-            return(model);
+            return (model);
 
-        }catch (Exception e) {
-            System.out.println("ERROR: "+e.getMessage());
-            return(null);
-        }
-    }
-    
-    public static void remove(String table, String column, String keyword, String username, String password){
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/bike_shop", username, password);
-            String query = "DELETE FROM "+table;
-            if (!(keyword.equals(""))){
-                query = "DELETE FROM "+table+" WHERE "+column+"='"+keyword+"'";
-            }
-            PreparedStatement preparedStatement = connection.prepareStatement(query);
-            preparedStatement.execute();
-            preparedStatement.close();
-            connection.close();
-
-        }catch (Exception e) {
-            System.out.println("ERROR: "+e.getMessage());
+        } catch (Exception e) {
+            System.out.println("ERROR: " + e.getMessage());
+            return (null);
         }
     }
 
 }
-
-
